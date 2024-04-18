6983a43e628e157866c1006e4b7a9e89e665c28b
aronamao <amao.arond@gmail.com>
2024-04-18 20:43:03 +0200

    updated locomotion logic and added walk

--- unity/earth-day-jam-2024/Assets/Scripts/PlayerAnim.cs
+++ unity/earth-day-jam-2024/Assets/Scripts/PlayerAnim.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
-    private Vector3 _lastPos;
     private Animator _animator;
-
-    private Transform _player;
 
     private static readonly int Speed = Animator.StringToHash("Speed");
 
     // Start is called before the first frame update
     void Start()
     {
-        _player = transform.parent;
         _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float speed = Vector3.Distance(_player.localPosition, _lastPos);
-        _animator.SetFloat(Speed, speed);
-        print(speed);
-        
-        _lastPos = _player.localPosition;
+        _animator.SetFloat(Speed, Input.GetAxis("Vertical"));
     }
 }
