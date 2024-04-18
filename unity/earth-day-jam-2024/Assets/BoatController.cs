ec8aa4680aca13985fc34bc1872d08b57d84aa20
Rob Adams <34306797+RobAdamsDev@users.noreply.github.com>
2024-04-18 17:12:18 +0200

    Rotation Bug Fix
    Now using Pro Builder Boat blockout. No longer has rotation issues

--- unity/earth-day-jam-2024/Assets/BoatController.cs
+++ unity/earth-day-jam-2024/Assets/BoatController.cs
@@ -10,7 +10,6 @@ public class BoatController : MonoBehaviour
     public float SteerPower = 500f;
     public float Power = 5f;
     public float MaxSpeed = 10f;
-    public float Drag = 0.1f;
 
     // Components
     protected Rigidbody rigidbody;
@@ -50,10 +49,8 @@ public class BoatController : MonoBehaviour
         // forward/backward acceleration
         if (Input.GetKey(KeyCode.W))
             rigidbody.AddForce(forward * (MaxSpeed * Power), ForceMode.Force);
-            // ApplyForceToReachVelocity(rigidbody, forward * MaxSpeed, Power);
         if (Input.GetKey(KeyCode.S))
             rigidbody.AddForce(forward * (-MaxSpeed * Power), ForceMode.Force);
-            // ApplyForceToReachVelocity(rigidbody, forward * -MaxSpeed, Power);
     }
 
     // Start is called before the first frame update
@@ -66,30 +63,6 @@ public class BoatController : MonoBehaviour
     void Update()
     {
         
-    }
-
-
-    // this function is from the YT video's GitHub repo and he doesn't explain any of it lol
-    public static void ApplyForceToReachVelocity(Rigidbody rigidbody, UnityEngine.Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
-    {
-        if (force == 0 || velocity.magnitude == 0)
-            return;
-
-        velocity = velocity + velocity.normalized * 0.2f * rigidbody.drag;
-
-        //force = 1 => need 1 s to reach velocity (if mass is 1) => force can be max 1 / Time.fixedDeltaTime
-        force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
-
-        //dot product is a projection from rhs to lhs with a length of result / lhs.magnitude https://www.youtube.com/watch?v=h0NJK4mEIJU
-        if (rigidbody.velocity.magnitude == 0)
-        {
-            rigidbody.AddForce(velocity * force, mode);
-        }
-        else
-        {
-            var velocityProjectedToTarget = (velocity.normalized * UnityEngine.Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude);
-            rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
-        }
     }
 
 }
