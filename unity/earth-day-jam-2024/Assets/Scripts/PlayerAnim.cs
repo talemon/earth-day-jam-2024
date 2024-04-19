d6863028f2ac81ba3225a8895617bae5d2804946
aronamao <amao.arond@gmail.com>
2024-04-19 18:50:53 +0200

    added steering animations

--- unity/earth-day-jam-2024/Assets/Scripts/PlayerAnim.cs
+++ unity/earth-day-jam-2024/Assets/Scripts/PlayerAnim.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     public Animator animator;
+    public Animator steeringWheelAnimator;
     private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int TurnDirection = Animator.StringToHash("TurnDirection");
     private static readonly int EnterSteeringAnim = Animator.StringToHash("EnterSteering");
     private static readonly int ExitSteeringAnim= Animator.StringToHash("ExitSteering");
 
@@ -18,15 +20,19 @@ public class PlayerAnim : MonoBehaviour
     void FixedUpdate()
     {
         animator.SetFloat(Speed, Math.Abs(Input.GetAxis("Vertical")));
+        animator.SetFloat(TurnDirection, Input.GetAxis("Horizontal"));
+        steeringWheelAnimator.SetFloat(TurnDirection, Input.GetAxis("Horizontal"));
     }
 
     public void EnterSteering()
     {
         animator.SetTrigger(EnterSteeringAnim);
+        steeringWheelAnimator.SetTrigger(EnterSteeringAnim);
     }
 
     public void ExitSteering()
     {
         animator.SetTrigger(ExitSteeringAnim);
+        steeringWheelAnimator.SetTrigger(ExitSteeringAnim);
     }
 }
