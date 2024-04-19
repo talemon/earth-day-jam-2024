650322ff0910bae1313603c07dca6e28a0b3cc78
Can Mert Bozkurt <admin@talemon.com>
2024-04-19 23:28:09 +0200

    Implemented animations for the main menu, allowed ship name input.

--- unity/earth-day-jam-2024/Assets/Scripts/MenuScripts/MainMenu.cs
+++ unity/earth-day-jam-2024/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,36 +10,110 @@ namespace MenuScripts
     public class MainMenu : MonoBehaviour
     {
         [SerializeField] private GameStateManager gameStateManager;
+
+        [SerializeField] private RectTransform[] mainTransforms;
+        [SerializeField] private RectTransform[] playMenuTransforms;
+
+        [SerializeField] private TMP_InputField shipNameInput;
+        [SerializeField] private Button startButton;
         
-        [SerializeField] private Button playButton;
-        [SerializeField] private Button creditsButton;
-        [SerializeField] private Button exitButton;
+        [SerializeField] private float animationTime = 1f;
+        [SerializeField] private float animationInterval = 1f;
 
         [SerializeField] private SceneAsset playScene;
     
         private void Start()
         {
-            playButton.onClick.AddListener(OnPlayClicked);
-            creditsButton.onClick.AddListener(OnCreditsClicked);
-            exitButton.onClick.AddListener(OnExitClicked);
+            DOTween.Init(false, true, LogBehaviour.Default);
+
+            var screenWidth = Screen.width;
+
+            foreach (var childTransform in mainTransforms)
+            {
+                MoveOut(childTransform);
+            }
+            foreach (var childTransform in playMenuTransforms)
+            {
+                MoveOut(childTransform);
+            }
             
-            Debug.Log(gameStateManager.GetGameState().ShipName);
+            AnimateIn(mainTransforms);
+
+            return;
+
+            void MoveOut(RectTransform t)
+            {
+                var pos = t.anchoredPosition;
+                pos.x = screenWidth;
+                t.anchoredPosition = pos;
+            }
         }
 
-        private void OnPlayClicked()
+        public void OnPlayClicked()
+        {
+            Sequence seq = DOTween.Sequence();
+            seq.Append(AnimateOut(mainTransforms));
+            seq.Append(AnimateIn(playMenuTransforms));
+        }
+
+        public void OnStartClicked()
         {
             gameStateManager.Initialize();
+            gameStateManager.GetGameState().ShipName = shipNameInput.text;
             SceneManager.LoadScene(playScene.name);
         }
 
-        private void OnCreditsClicked()
+        public void OnCreditsClicked()
         {
         
         }
 
-        private void OnExitClicked()
+        public void OnExitClicked()
         {
             Application.Quit();
+        }
+
+        public void OnPlayBackClicked()
+        {
+            Sequence seq = DOTween.Sequence();
+            seq.Append(AnimateOut(playMenuTransforms));
+            seq.Append(AnimateIn(mainTransforms));
+        }
+
+        public void OnShipNameChanged(string newInput)
+        {
+            startButton.enabled = !string.IsNullOrEmpty(newInput);
+        }
+        
+        private Sequence AnimateOut(RectTransform[] transforms)
+        {
+            Sequence seq = DOTween.Sequence();
+            
+            var width = Screen.width;
+            var interval = animationInterval;
+            foreach (var child in transforms)
+            {
+                var tween = child.DOAnchorPosX(width, animationTime).SetEase(Ease.InBack);
+                seq.Insert(interval, tween);
+                interval += animationInterval;
+            }
+            
+            return seq;
+        }
+
+        private Sequence AnimateIn(RectTransform[] transforms)
+        {
+            Sequence seq = DOTween.Sequence();
+            
+            var interval = animationInterval;
+            foreach (var child in transforms)
+            {
+                var tween = child.DOAnchorPosX(0f, animationTime).SetEase(Ease.OutBack);
+                seq.Insert(interval, tween);
+                interval += animationInterval;
+            }
+
+            return seq;
         }
     }
 }
