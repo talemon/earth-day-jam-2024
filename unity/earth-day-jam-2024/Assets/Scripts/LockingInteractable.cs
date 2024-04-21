17823fcd54b7c9af465fda6789ba5118a49aa263
DmitryTsyganov <dmitrytsgn@gmail.com>
2024-04-21 16:34:02 +0200

    Small interactables refactor Additional harpoon assets

---
+++ unity/earth-day-jam-2024/Assets/Scripts/LockingInteractable.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class LockingInteractable : InteractableObject
+{
+    [SerializeField] protected Transform playerLockPosition;
+    [SerializeField] protected GameplayCameraController cameraController;
+
+    private Transform _playerParent;
+    
+    protected override bool CanInteract()
+    {
+        return enabled;
+    }
+
+    protected override void OnInteract(GameObject player)
+    {
+        base.OnInteract(player);
+        if (State == InteractableObjectState.Available)
+        {
+            State = InteractableObjectState.Busy;
+            
+            CapturePlayer(player);
+        }
+        else if(State == InteractableObjectState.Busy)
+        {
+            State = InteractableObjectState.Available;
+            
+            ReleasePlayer(player);
+        }
+    }
+
+    protected void CapturePlayer(GameObject player)
+    {
+        var interactionComp = player.GetComponent<PlayerInteractionComponent>();
+        interactionComp.State = PlayerInteractionState.Locked;
+
+        var playerController = player.GetComponent<PlayerController>();
+        playerController.enabled = false;
+
+        _playerParent = player.transform.parent;
+        player.transform.SetParent(playerLockPosition);
+        player.transform.SetPositionAndRotation(playerLockPosition.position, playerLockPosition.rotation);
+    }
+
+    protected void ReleasePlayer(GameObject player)
+    {
+        player.transform.SetParent(_playerParent);
+        
+        var playerController = player.GetComponent<PlayerController>();
+        playerController.enabled = true;
+        
+        var interactionComp = player.GetComponent<PlayerInteractionComponent>();
+        interactionComp.State = PlayerInteractionState.Free;
+    }
+}
--- unity/earth-day-jam-2024/Assets/Scripts/ShipWheelInteractable.cs
+++ unity/earth-day-jam-2024/Assets/Scripts/ShipWheelInteractable.cs
@@ -1,71 +1,33 @@
 ﻿using UnityEngine;
 
-public class ShipWheelInteractable : InteractableObject
+public class ShipWheelInteractable : LockingInteractable
 {
-    [SerializeField] private Transform captainSeat;
     [SerializeField] private KinematicBoat boatController;
-    [SerializeField] private GameplayCameraController cameraController;
-
-    private Transform _playerParent;
-    
-    protected override bool CanInteract()
-    {
-        return enabled;
-    }
 
     protected override void OnInteract(GameObject player)
     {
         if (State == InteractableObjectState.Available)
         {
-            State = InteractableObjectState.Busy;
-            
-            CapturePlayer(player);
             boatController.enabled = true;
             cameraController.SwitchToCamera(EGameplayCamera.Boat);
+
+            var playerAnim = player.GetComponentInChildren<PlayerAnim>();
+            if (playerAnim != null)
+            {
+                playerAnim.EnterSteering();
+            }
         }
         else if(State == InteractableObjectState.Busy)
         {
-            State = InteractableObjectState.Available;
-            
-            ReleasePlayer(player);
             boatController.enabled = false;
             cameraController.SwitchToCamera(EGameplayCamera.Player);
+
+            var playerAnim = player.GetComponentInChildren<PlayerAnim>();
+            if (playerAnim != null)
+            {
+                playerAnim.ExitSteering();
+            }
         }
-    }
-
-    private void CapturePlayer(GameObject player)
-    {
-        var interactionComp = player.GetComponent<PlayerInteractionComponent>();
-        interactionComp.State = PlayerInteractionState.Locked;
-
-        var playerController = player.GetComponent<PlayerController>();
-        playerController.enabled = false;
-
-        _playerParent = player.transform.parent;
-        player.transform.SetParent(captainSeat);
-        player.transform.SetPositionAndRotation(captainSeat.position, captainSeat.rotation);
-
-        var playerAnim = player.GetComponentInChildren<PlayerAnim>();
-        if (playerAnim != null)
-        {
-            playerAnim.EnterSteering();
-        }
-    }
-
-    private void ReleasePlayer(GameObject player)
-    {
-        player.transform.SetParent(_playerParent);
-        
-        var playerController = player.GetComponent<PlayerController>();
-        playerController.enabled = true;
-        
-        var interactionComp = player.GetComponent<PlayerInteractionComponent>();
-        interactionComp.State = PlayerInteractionState.Free;
-        
-        var playerAnim = player.GetComponentInChildren<PlayerAnim>();
-        if (playerAnim != null)
-        {
-            playerAnim.ExitSteering();
-        }
+        base.OnInteract(player);
     }
 }
